0d891ff35a26dadab6725de4b1aada7d291dfd54
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-10-07 10:30:04 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,1 +1,4 @@
-print("Hello! My name is MiMi \nI was created in 2021")
+print("Hello! My name is MiMi. \nI was created in 2021.")
+print("Pleas, remind me your name.")
+name_1 = input()
+print("What a great name you have, " +name_1+ "!")
