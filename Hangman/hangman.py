669184eeda51f6b7b8f9cc3030b8e1718ed3ff0b
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-10-21 15:06:35 +0300

    Hangman 2-nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,2 +1,7 @@
 print("HANGMAN")
-print("The game will b available soon.")
+print("The game will be available soon.")
+hidden_word = str(input("Guess the word: "))
+if hidden_word == ("python"):
+    print("You survived!")
+else:
+    print("You lost!")
