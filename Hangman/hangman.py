e07dadc35a1d6e73d8ec5a6d04602f7834675a75
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-10-27 20:20:14 +0300

    Hangman 4-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,9 +1,12 @@
 print("HANGMAN")
 print("The game will be available soon.")
 import random
-hidden_word = str(input("Guess the word: "))
 word_list = ["python", "java", "javascript", "php"]
 word_r = random.choice(word_list)
+word_1 = list(word_r)
+word_2 = word_1[0:3]
+word_3 = "".join(word_2)
+hidden_word = str(input("Guess the word " + word_3 + ": "))
 if hidden_word == word_r:
     print("You survived!")
 else:
