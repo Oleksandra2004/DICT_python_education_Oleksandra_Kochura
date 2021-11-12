57745d4072f5989c11ff03c6ab1affb90c9efcde
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-11-12 17:14:59 +0200

    Hangman 6-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,21 +1,31 @@
 import random
+
 print("HANGMAN")
 words = ["python", "java", "javascript", "php"]
-word_r = random.choice(words)
-word_list = list(word_r)
-str_null = len(word_r) * "-"
-list_null = list(str_null)
-for i in range(10):
+word_r = random.choice(words)  # python
+word_list = list(word_r)  # [p, y, t, h, o, n]
+encrypt_str = len(word_r) * "-"  # ------
+encrypt_list = list(encrypt_str)  # [-, -, -, -, -, -]
+list_null = []
+print(encrypt_str)
+for i in range(8):
     enter_letter = str(input("Input a letter: "))
+    if enter_letter in list_null:
+        print("No improvements")
+    list_null.append(enter_letter)
     if enter_letter in word_list:
         if word_list.count(enter_letter) >= 2:
             index = word_list.index(enter_letter)
-            list_null[index] = enter_letter
+            encrypt_list[index] = enter_letter
             word_list[index] = "-"
         index = word_list.index(enter_letter)
-        list_null[index] = enter_letter
-    else:
+        encrypt_list[index] = enter_letter
+    elif enter_letter not in word_list:
         print("That letter doesn't appear in the word")
-    print(''.join(list_null))
-print("Thanks for playing!")
-print("We'll see how well you did in the next stage")
+    word_str = ''.join(encrypt_list)
+    print(''.join(encrypt_list))
+word_str = ''.join(encrypt_list)
+if word_str == word_r:
+    print("You guessed the word! \nYou survived!")
+else:
+    print("No improvements \nYou lost!")
