33adce78b9ba32a5cec4fa7552d7a19de9fa8482
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-12-22 20:13:54 +0200

    CoffeeMachine 2-th stage

--- CoffeeMachine/coffeemachine.py
+++ CoffeeMachine/coffeemachine.py
@@ -1,10 +1,13 @@
-def cooking():
-    print("Starting to make a coffee")
-    print("Grinding coffee beans")
-    print("Boiling water")
-    print("Mixing boiled water with crushed coffee beans")
-    print("Pouring coffee into the cup")
-    print("Coffee is ready!")
+def coffee():
+    print("Write how many cups of coffee you will need: ")
+    many_cups = int(input("> "))
+    water = many_cups * 200
+    milk = many_cups * 50
+    coffee_beans = many_cups * 15
+    print(f"For {many_cups} cups of coffee you will need:")
+    print(f"{water} ml of water")
+    print(f"{milk} ml of milk")
+    print(f"{coffee_beans} g of coffee beans")
 
 
-cooking()
+coffee()
