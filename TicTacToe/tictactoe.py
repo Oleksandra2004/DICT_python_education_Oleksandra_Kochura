95bb6b13e10980873d9cfffc0bf1d62704782800
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-12-22 20:36:45 +0200

    TicTacToe 4-th stage

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -6,8 +6,8 @@ def game_grid(matrix_):
     print("---------")
 
 
-user = input("Enter cells: ")
-row_list = [[j for j in user[i:i+3]] for i in range(0, 7, 3)]
+user = input("Enter cells:")
+row_list = [[j for j in user[i:i + 3]] for i in range(0, 7, 3)]
 col_list = [[j for j in user[i::3]] for i in range(3)]
 diagonal_list = [[row_list[i][i] for i in range(3)]] + [[row_list[i][2 - i] for i in range(3)]]
 matrix = row_list + col_list + diagonal_list
@@ -18,13 +18,19 @@ o_win = ["O", "O", "O"]
 count_x = user.count("X")
 count_o = user.count("O")
 
-if (o_win in matrix and x_win in matrix) or abs(count_x - count_o) > 1:
-    print("Impossible")
-elif o_win in matrix:
-    print("X win")
-elif x_win in matrix:
-    print("O win")
-elif "_" in user:
-    print("Game not finished")
-else:
-    print("Draw")
+valid = False
+while not valid:
+    row, col = input("Enter the coordinates: ").split()
+    try:
+        row, col = int(row), int(col)
+        if 1 <= row <= 3 and 1 <= col <= 3:
+            if "_" in row_list[row - 1][col - 1]:
+                row_list[row - 1][col - 1] = "X"
+                game_grid(row_list)
+                valid = True
+            else:
+                print("This cell is occupied! Choose another one!")
+        else:
+            print("Coordinates should be from 1 to 3!")
+    except ValueError:
+        print("You should enter numbers!")
