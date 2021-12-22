a6b4c0c6f7e717004a94bbdd9eb9b7b6e83b0997
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-12-22 20:30:41 +0200

    TicTacToe 2-th stage

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,7 +1,13 @@
+a = "---------------"
+
+
 def play():
-    print("X O X")
-    print("O O X")
-    print("X X O")
+    user = list(input("Enter cells: "))
+    print(a)
+    print(f"| ", user[0], " ", user[1], " ", user[2], " |")
+    print(f"| ", user[3], " ", user[4], " ", user[5], " |")
+    print(f"| ", user[6], " ", user[7], " ", user[8], " |")
+    print(a)
 
 
 play()
