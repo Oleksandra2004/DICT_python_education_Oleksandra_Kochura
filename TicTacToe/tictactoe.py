608610e85fa21ee3bb58c80e12d3de778742e7e0
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-12-22 20:34:07 +0200

    TicTacToe 3-th stage

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,13 +1,30 @@
-a = "---------------"
+def game_grid(matrix_):
+    print("---------")
+    print("|", *matrix_[0], "|")
+    print("|", *matrix_[1], "|")
+    print("|", *matrix_[2], "|")
+    print("---------")
 
 
-def play():
-    user = list(input("Enter cells: "))
-    print(a)
-    print(f"| ", user[0], " ", user[1], " ", user[2], " |")
-    print(f"| ", user[3], " ", user[4], " ", user[5], " |")
-    print(f"| ", user[6], " ", user[7], " ", user[8], " |")
-    print(a)
+user = input("Enter cells: ")
+row_list = [[j for j in user[i:i+3]] for i in range(0, 7, 3)]
+col_list = [[j for j in user[i::3]] for i in range(3)]
+diagonal_list = [[row_list[i][i] for i in range(3)]] + [[row_list[i][2 - i] for i in range(3)]]
+matrix = row_list + col_list + diagonal_list
+game_grid(matrix)
 
+x_win = ["X", "X", "X"]
+o_win = ["O", "O", "O"]
+count_x = user.count("X")
+count_o = user.count("O")
 
-play()
+if (o_win in matrix and x_win in matrix) or abs(count_x - count_o) > 1:
+    print("Impossible")
+elif o_win in matrix:
+    print("X win")
+elif x_win in matrix:
+    print("O win")
+elif "_" in user:
+    print("Game not finished")
+else:
+    print("Draw")
