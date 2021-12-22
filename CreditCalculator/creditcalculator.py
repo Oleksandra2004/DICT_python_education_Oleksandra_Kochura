5a9457009bf0fe7f8dcb0d942cf2e340a18fa540
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-12-22 20:47:02 +0200

    CreditCalculator 2-th stage

--- CreditCalculator/creditcalculator.py
+++ CreditCalculator/creditcalculator.py
@@ -1,2 +1,38 @@
-print("Loan principal: 1000\nMonth 1: repaid 250\nMonth 2: "
-      "repaid 250\nMonth 3: repaid 500\nThe loan has been repaid!")
+import math
+
+
+principal = int(input("Enter the loan principal:\n> "))
+print("What do you want to calculate?")
+
+
+def monthly_payment(months):
+    payment = math.ceil(principal / months)
+    print(f"It will take {payment} months to repay the loan")
+
+
+def periods_payment(periods):
+    period_payment = math.ceil(principal / periods)
+    last_payment = principal - (periods - 1) * period_payment
+    print(f"Your monthly payment = {period_payment}" if period_payment == last_payment
+          else f"Your monthly payment = {period_payment} and the last payment = {last_payment}")
+
+
+def init():
+    user = input("type 'm' - for number of monthly payments,\n"
+                 "type 'p' - for the monthly payment:\n> ")
+    main(user)
+
+
+def main(user_input):
+    if user_input == "m":
+        months = int(input("Enter the monthly payment:\n> "))
+        monthly_payment(months)
+    elif user_input == "p":
+        periods = int(input("Enter the number of months:\n> "))
+        periods_payment(periods)
+    else:
+        print("You type incorrect word")
+        init()
+
+
+init()
