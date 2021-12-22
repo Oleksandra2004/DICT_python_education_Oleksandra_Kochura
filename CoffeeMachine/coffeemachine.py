105b5a34442d72dcd89bf5c71f0fa5467f16d8fb
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-12-22 20:15:26 +0200

    CoffeeMachine 3-th stage

--- CoffeeMachine/coffeemachine.py
+++ CoffeeMachine/coffeemachine.py
@@ -1,13 +1,37 @@
-def coffee():
-    print("Write how many cups of coffee you will need: ")
-    many_cups = int(input("> "))
-    water = many_cups * 200
-    milk = many_cups * 50
-    coffee_beans = many_cups * 15
-    print(f"For {many_cups} cups of coffee you will need:")
-    print(f"{water} ml of water")
-    print(f"{milk} ml of milk")
-    print(f"{coffee_beans} g of coffee beans")
+# переменные пользователя
+print("Write how many ml water the coffee machine has:")
+water_user = int(input("> "))
+print("Write how many ml milk the coffee machine has:")
+milk_user = int(input("> "))
+print("Write how many grams of coffee beans the coffee machine has:")
+beans_user = int(input("> "))
+print("Write how many cups of coffee you  will need:")
+cups_user = int(input("> "))
+
+# основные переменные
+water = 200
+milk = 50
+beans = 15
 
 
-coffee()
+def coffee_settings():
+    water_many = water_user // water
+    milk_many = milk_user // milk
+    beans_many = beans_user // beans
+    if water_many * milk_many * beans_many == 0:
+        cups = 0
+    else:
+        cups = min(water_many, milk_many, beans_many)
+
+    cups_extra = cups - cups_user
+
+    if cups_user == cups:
+        print("Yes, I can make that amount of coffee")
+    elif cups >= cups_user:
+        print("Yes, I can make that amount of coffee (and even "
+              + str(cups_extra) + " more than that)")
+    else:
+        print("No, I can make only " + str(cups) + " cups of coffee")
+
+
+coffee_settings()
