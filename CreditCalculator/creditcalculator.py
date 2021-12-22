c339e9d7d0fd4635b86f8a52b536aadcb3a234d4
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-12-22 21:27:08 +0200

    CreditCalculator 3-th stage

--- CreditCalculator/creditcalculator.py
+++ CreditCalculator/creditcalculator.py
@@ -1,35 +1,52 @@
 import math
 
 
-principal = int(input("Enter the loan principal:\n> "))
-print("What do you want to calculate?")
+def number_monthly():
+    p = float(input("Enter the loan principal:\n> "))
+    m = float(input("Enter the monthly payment:\n> "))
+    i = float(input("Enter the loan interest:\n> ")) / 1200
+    num = (m / (m - (i * p)))
+    number_1 = math.log(num, (1 + i))
+    number = math.ceil(number_1)
+    if number % 12 != 0:
+        print(f"It will take {number // 12} years and {number % 12} months to repay this loan!")
+    elif number % 12 == 0:
+        print(f"It will take {number / 12} years to repay this loan!")
+    else:
+        print(f"It will take {number} months to repay this loan!")
 
 
-def monthly_payment(months):
-    payment = math.ceil(principal / months)
-    print(f"It will take {payment} months to repay the loan")
+def loan():
+    a = float(input("Enter the annuity payment:\n> "))
+    n = float(input("Enter the number of periods:\n> "))
+    i = float(input("Enter the loan interest:\n> ")) / 1200
+    num = a / ((i * math.pow((1 + i), n)) / (math.pow((1 + i), n) - 1))
+    print(f"Your loan principal = {round(num)}!")
 
 
-def periods_payment(periods):
-    period_payment = math.ceil(principal / periods)
-    last_payment = principal - (periods - 1) * period_payment
-    print(f"Your monthly payment = {period_payment}" if period_payment == last_payment
-          else f"Your monthly payment = {period_payment} and the last payment = {last_payment}")
+def annuity_monthly():
+    p = int(input("Enter the loan principal:\n> "))
+    n = int(input("Enter the number of periods:\n> "))
+    i = int(input("Enter the loan interest:\n> ")) / 1200
+    num = p * ((i * math.pow((1 + i), n)) / (math.pow((1 + i), n) - 1))
+    print(f"Your monthly payment = {math.ceil(num)}!")
 
 
 def init():
-    user = input("type 'm' - for number of monthly payments,\n"
-                 "type 'p' - for the monthly payment:\n> ")
+    print("What do you want to calculate?")
+    user = input("type 'n' for number of monthly payments,\n"
+                 "type 'a' for annuity monthly payment amount,\n"
+                 "type 'p' for the loan principal:\n> ")
     main(user)
 
 
 def main(user_input):
-    if user_input == "m":
-        months = int(input("Enter the monthly payment:\n> "))
-        monthly_payment(months)
+    if user_input == "n":
+        number_monthly()
     elif user_input == "p":
-        periods = int(input("Enter the number of months:\n> "))
-        periods_payment(periods)
+        loan()
+    elif user_input == "a":
+        annuity_monthly()
     else:
         print("You type incorrect word")
         init()
