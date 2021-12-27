cc793e3bc63c513079fa67d35b6e57a5633c8bd4
Oleksandra Kochura <o.r.kochura@student.khai.edu>
2021-12-27 15:00:41 +0200

    Rock-Paper-Scissors 2-th stage

--- Rock-Paper-Scissors/rock-paper-scissors.py
+++ Rock-Paper-Scissors/rock-paper-scissors.py
@@ -1,15 +1,29 @@
-def option(option_):
-    print(f"Sorry, but the computer chose {option_}")
+import random
+
+
+def win(a, b):
+    p = "paper"
+    r = "rock"
+    s = "scissors"
+    if (a == p and b == r) or (a == s and b == p) or (a == r and b == s):
+        print(f"Well done. The computer chose {b} and failed")
+    else:
+        print(f"Sorry, but the computer chose {b}")
 
 
 def main():
-    user = input("> ")
-    if user == "scissors":
-        option("rock")
-    elif user == "paper":
-        option("scissors")
-    elif user == "rock":
-        option("paper")
+    try:
+        option = ["rock", "scissors", "paper"]
+        option_r = random.choice(option)
+        user = input("> ")
+        msg = "You should enter 'rock', 'paper' or 'scissors'"
+        assert user in option, msg
+        if user == option_r:
+            print(f"There is a draw ({option_r})")
+        else:
+            win(user, option_r)
+    except AssertionError as error:
+        print(error)
 
 
 if __name__ == '__main__':
